72a1551006f7ced80539e1c51bdac3cbf37f2407
mooujarrar <mohyiddine1997@gmail.com>
2023-12-14 14:25:51 +0100

    init; initial init

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Plants Microservice')
+    .setDescription('Test the plants microservice')
+    .setVersion('1.0')
+    .addTag('plants')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
