326dacb70922472083284f581d206dc88e104f3c
mooujarrar <mohyiddine1997@gmail.com>
2023-12-14 18:45:36 +0100

    maint; Dependency microservices

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('Plants Microservice')
-    .setDescription('Test the plants microservice')
-    .setVersion('1.0')
-    .addTag('plants')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      port: 3001
+    }
+  });
+  await app.startAllMicroservices();
+  await app.listen(3001);
 }
 bootstrap();
