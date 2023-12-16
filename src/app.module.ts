a703d79cede38d4b90a4b1cbc7de2ba41103c5eb
mooujarrar <mohyiddine1997@gmail.com>
2023-12-16 16:15:39 +0100

    enh#introduce MySQL through docker, make configuration in .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PlantModule } from './plant/plant.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PlantModule],
+  imports: [
+    PlantModule,
+    ConfigModule.forRoot()
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/db/database.providers.ts
+++ src/db/database.providers.ts
@@ -1,17 +1,19 @@
 import { DataSource } from 'typeorm';
-import { DATA_SOURCE_TOKEN } from '../helpers/constants';
+import { DATA_SOURCE_TOKEN } from '../tokens';
+import { ConfigService } from '@nestjs/config';
 
 export const databaseProviders = [
   {
     provide: DATA_SOURCE_TOKEN,
-    useFactory: async () => {
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => {
       const dataSource = new DataSource({
-        type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'root',
-        database: 'test',
+        type: configService.get<string>('DB_TYPE') as "mysql" | "mariadb",
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('MYSQL_USER'),
+        password: configService.get<string>('MYSQL_ROOT_PASSWORD'),
+        database: configService.get<string>('MYSQL_DATABASE'),
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       });
