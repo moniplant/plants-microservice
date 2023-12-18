64c0cfe19689b9a12ae28532be8b1c0c228e32a8
mooujarrar <mohyiddine1997@gmail.com>
2023-12-18 09:34:26 +0100

    bug#Fix bug, event messages not reaching from gateway to the microservice

--- src/plant/plant.module.ts
+++ src/plant/plant.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { plantProviders } from './plant.providers';
 import { PlantService } from './plant.service';
 import { DatabaseModule } from '../db/database.module';
+import { PlantController } from './plant.controller';
 
 @Module({
   imports: [DatabaseModule],
+  controllers: [PlantController],
   providers: [...plantProviders, PlantService],
 })
 export class PlantModule {}
