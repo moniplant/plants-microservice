fd4e883874387e6bde146ef6b281d0a29fe5cac8
mooujarrar <mohyiddine1997@gmail.com>
2023-12-18 12:32:07 +0100

    enh# extend events list

--- src/events.ts
+++ src/events.ts
@@ -1,1 +1,4 @@
 export const CREATE_PLANT = 'plant.create';
+export const DELETE_PLANT = 'plant.delete';
+export const UPDATE_PLANT = 'plant.delete';
+export const RETRIEVE_PLANT = 'plant.retrieve';
