a41b0c49ff44b4c52cd3fc30cb3e4caae35ffc38
mooujarrar <mohyiddine1997@gmail.com>
2024-05-12 19:11:59 +0200

    maint; documentation and lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,6 @@ import { PlantModule } from './plant/plant.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    PlantModule,
-    ConfigModule.forRoot()
-  ],
+  imports: [PlantModule, ConfigModule.forRoot()],
 })
-export class AppModule { }
+export class AppModule {}
