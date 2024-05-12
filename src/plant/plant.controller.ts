8c4f90cc18b22e38ebee5a39e11697533005f4f6
mooujarrar <mohyiddine1997@gmail.com>
2024-05-12 19:08:53 +0200

    maint; fix inter service communication with docker

--- src/plant/plant.controller.ts
+++ src/plant/plant.controller.ts
@@ -1,5 +1,5 @@
 // src/plant/plant.controller.ts
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { PlantService } from './plant.service';
 import { EventPattern, Payload } from '@nestjs/microservices';
 import { CREATE_PLANT } from 'src/events';
@@ -10,6 +10,7 @@ export class PlantController {
 
   @EventPattern(CREATE_PLANT)
   handleCreatePlant(@Payload() data: any) {
+    Logger.log('Received new payload: ', data);
     this.plantService.handleCreatePlant(data);
   }
 }
