59d518dd23876303583e69b868ea493191152c6c
mooujarrar <mohyiddine1997@gmail.com>
2024-06-13 18:01:21 +0200

    maint; add new kafka topic for sensors table in the plants microservice

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { PLANTS_CONSUMER_GROUP_ID } from './tokens';
+import { PLANTS_CONSUMER_GROUP_ID, SENSORS_CONSUMER_GROUP_ID } from './tokens';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.KAFKA,
-      options: {
-        client: {
-          brokers: ['kafka:29092'],
-        },
-        consumer: {
-          groupId: PLANTS_CONSUMER_GROUP_ID,
-        },
+  const app = await NestFactory.create(AppModule);
+  // Microservice #1: managing data from sensors
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['kafka:29092'],
+      },
+      consumer: {
+        groupId: PLANTS_CONSUMER_GROUP_ID,
       },
     },
-  );
-  app.listen();
+  });
+  // Microservice #2: managing sensors (Creation, update etc...)
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['kafka:29092'],
+      },
+      consumer: {
+        groupId: SENSORS_CONSUMER_GROUP_ID,
+      },
+    },
+  });
+  await app.startAllMicroservices();
 }
 bootstrap();
