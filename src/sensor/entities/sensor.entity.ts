b6eb9c34c6577248c00d65e0a138e119951c689e
mooujarrar <mohyiddine1997@gmail.com>
2024-06-13 19:39:20 +0200

    maint; added entity sensor

--- src/sensor/entities/sensor.entity.ts
+++ src/sensor/entities/sensor.entity.ts
@@ -1,1 +1,25 @@
-export class Sensor {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Plant } from '../../plant/plant.entity';
+
+@Entity()
+export class Sensor {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  label: string;
+
+  @Column()
+  quantity: string;
+
+  @Column()
+  unit: string;
+
+  @Column({ nullable: true })
+  plantId: number;
+
+  @ManyToOne(() => Plant, (plant) => plant.sensors, {
+    onDelete: 'SET NULL',
+  })
+  plant: Plant;
+}
--- src/sensor/sensor.module.ts
+++ src/sensor/sensor.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SensorService } from './sensor.service';
 import { SensorController } from './sensor.controller';
+import { Sensor } from './entities/sensor.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Sensor])],
   controllers: [SensorController],
   providers: [SensorService],
 })
