c1947aa7437f51b3e246bd370d9bb3b642ebe64f
mooujarrar <mohyiddine1997@gmail.com>
2025-02-09 21:52:16 +0100

    enh# Add right events to the sensor controller

--- src/sensor/sensor.controller.ts
+++ src/sensor/sensor.controller.ts
@@ -3,32 +3,39 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import { SensorService } from './sensor.service';
 import { CreateSensorDto } from './dto/create-sensor.dto';
 import { UpdateSensorDto } from './dto/update-sensor.dto';
+import {
+  CREATE_SENSOR,
+  DELETE_SENSOR,
+  LIST_PLANT_SENSORS,
+  RETRIEVE_SENSOR,
+  UPDATE_SENSOR,
+} from '../events';
 
 @Controller()
 export class SensorController {
   constructor(private readonly sensorService: SensorService) {}
 
-  @MessagePattern('createSensor')
+  @MessagePattern(CREATE_SENSOR)
   create(@Payload() createSensorDto: CreateSensorDto) {
     return this.sensorService.create(createSensorDto);
   }
 
-  @MessagePattern('findAllSensor')
+  @MessagePattern(LIST_PLANT_SENSORS)
   findAll() {
     return this.sensorService.findAll();
   }
 
-  @MessagePattern('findOneSensor')
+  @MessagePattern(RETRIEVE_SENSOR)
   findOne(@Payload() id: number) {
     return this.sensorService.findOne(id);
   }
 
-  @MessagePattern('updateSensor')
+  @MessagePattern(UPDATE_SENSOR)
   update(@Payload() updateSensorDto: UpdateSensorDto) {
     return this.sensorService.update(updateSensorDto.id, updateSensorDto);
   }
 
-  @MessagePattern('removeSensor')
+  @MessagePattern(DELETE_SENSOR)
   remove(@Payload() id: number) {
     return this.sensorService.remove(id);
   }
