8193626d7c2417e3f94b23ea27a998f74c22763e
mooujarrar <mohyiddine1997@gmail.com>
2025-04-26 00:55:52 +0200

    maint; move to external address port

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
       transport: Transport.KAFKA,
       options: {
         client: {
-          brokers: ['kafka:29092'],
+          brokers: ['kafka:9092'],
         },
         consumer: {
           groupId: PLANTS_CONSUMER_GROUP_ID,
